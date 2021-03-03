4ab27f6497a96ce67ca517d25a01e663dd724b6d
Tarun Sharma <tarun7582@gmail.com>
2021-03-03 07:29:09 +0530

    it better

--- routes/index.js
+++ routes/index.js
@@ -360,22 +360,22 @@ router.post('/getCount', dataCountCtrl.getCount);
 
 
 
-net = require("net");
+// net = require("net");
 
-net.createServer((socket) => {
-  socket.on("error", (err) => {
-    console.log("Caught flash policy server socket error: ")
-    console.log(err.stack)
-  });
-  socket.write("<?xml version=\"1.0\"?>\n");
-  socket.write("<!DOCTYPE cross-domain-policy SYSTEM \"http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd\">\n");
-  socket.write("<cross-domain-policy>\n");
-  socket.write("<allow-access-from domain=\"*\" to-ports=\"*\"/>\n");
-  socket.write("</cross-domain-policy>\n");
-  socket.end();
-}).listen(843, function () {
-  console.log('Socket ON')
-});
+// net.createServer((socket) => {
+//   socket.on("error", (err) => {
+//     console.log("Caught flash policy server socket error: ")
+//     console.log(err.stack)
+//   });
+//   socket.write("<?xml version=\"1.0\"?>\n");
+//   socket.write("<!DOCTYPE cross-domain-policy SYSTEM \"http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd\">\n");
+//   socket.write("<cross-domain-policy>\n");
+//   socket.write("<allow-access-from domain=\"*\" to-ports=\"*\"/>\n");
+//   socket.write("</cross-domain-policy>\n");
+//   socket.end();
+// }).listen(843, function () {
+//   console.log('Socket ON')
+// });
 
  
 
