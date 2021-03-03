06be1a7d0a2566393eb5c481912f9ae940fc2ac0
Tarun Sharma <tarun7582@gmail.com>
2021-03-03 07:33:03 +0530

    yehhhhhhhhhhhh!!!!!!

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+/**
+ * var port = process.env.PORT || 8080;
+ */
 
 // middleware
 
@@ -52,8 +54,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(port, function() {
-  console.log('Our app is running on http://localhost:' + port);
-});
+// app.listen(port, function() {
+//   console.log('Our app is running on http://localhost:' + port);
+// });
 
 module.exports = app;
